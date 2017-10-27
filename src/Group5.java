520064bf5064bc1eda43c8eef492b56de75bb1e1
Susobhan Ghosh <susobhang70@gmail.com>
2017-10-28 01:22:43 +0530

    Added opponent modelling

--- src/Group5.java
+++ src/Group5.java
@@ -1,17 +1,27 @@
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import negotiator.AgentID;
 import negotiator.Bid;
 import negotiator.Deadline;
+import negotiator.Domain;
 import negotiator.actions.Accept;
 import negotiator.actions.Action;
 import negotiator.actions.Offer;
 import negotiator.bidding.BidDetails;
 import negotiator.boaframework.SortedOutcomeSpace;
+import negotiator.issue.Issue;
+import negotiator.issue.IssueDiscrete;
+import negotiator.issue.ValueDiscrete;
+import negotiator.issue.Objective;
 import negotiator.parties.AbstractNegotiationParty;
 import negotiator.session.TimeLineInfo;
 import negotiator.utility.AbstractUtilitySpace;
+import negotiator.utility.AdditiveUtilitySpace;
+import negotiator.utility.Evaluator;
+import negotiator.utility.EvaluatorDiscrete;
 
 /**
  * This is your negotiation party.
@@ -20,22 +30,27 @@ import negotiator.utility.AbstractUtilitySpace;
 public class Group5 extends AbstractNegotiationParty
 {
 
+	private static final double defectDiscount = 0.9;
+
 	private double fDiscountFactor = 0;
 	private double fReservationValue = 0;
-
 	private double fBeta = 1.2;
 	
 	private List<BidDetails> lOutcomeSpace;
-	private int nRounds, nCurrentRound, nCount;
+	private List<Issue> lIssue; 
+	private HashMap<AgentID, AdditiveUtilitySpace> lAgentUtilSpaces;
+	private int nRounds, nCurrentRound, nCount, nIssues;
+	private int nRejectOffers;
 
 	private HashMap<AgentID, List<Bid>> bidHistory;
+	private HashMap<AgentID, Bid> highestOfferedBids;
 	private Bid lastBid;
+	private Domain domain;
 
 	@Override
 	public void init(AbstractUtilitySpace utilSpace, Deadline dl,
 			TimeLineInfo tl, long randomSeed, AgentID agentId)
 	{
-
 		super.init(utilSpace, dl, tl, randomSeed, agentId);
 
 		fDiscountFactor = utilSpace.getDiscountFactor(); // read discount factor
@@ -44,13 +59,25 @@ public class Group5 extends AbstractNegotiationParty
 
 		System.out.println("Reservation Value is " + fReservationValue);
 
+		lAgentUtilSpaces = new HashMap<AgentID, AdditiveUtilitySpace>();
+
 		lOutcomeSpace = new SortedOutcomeSpace(this.utilitySpace).getAllOutcomes();
+		domain = this.utilitySpace.getDomain();
+		lIssue = domain.getIssues();
 
 		bidHistory = new HashMap<AgentID, List<Bid>>();
+		highestOfferedBids = new HashMap<AgentID, Bid>();
 
 		nRounds = this.deadlines.getValue();
+		nIssues = lIssue.size();
 		nCurrentRound = 0;
 		nCount = 0;
+
+		// number of rounds to defect in order to read the preferences of other agents
+		if(fDiscountFactor != 1.0D)
+			nRejectOffers = (int) (Math.log(defectDiscount) / Math.log(fDiscountFactor)) + 1;
+		else
+			nRejectOffers = 50;
 	}
 
 	/**
@@ -69,7 +96,19 @@ public class Group5 extends AbstractNegotiationParty
 
 		if(nCurrentRound >= nRounds - 5 && getUtility(this.lastBid) > fReservationValue)
 			return new Accept();
+		
+		if(getUtility(this.lastBid) >= Group5.defectDiscount)
+			return new Accept();
 
+		if(nCurrentRound <= nRejectOffers)
+		{
+			SortedOutcomeSpace sortSpace = new SortedOutcomeSpace(this.utilitySpace);
+			Bid newbid = sortSpace.getBidNearUtility(Group5.defectDiscount).getBid();
+			this.lastBid = newbid;
+			return new Offer(newbid);
+		}
+		
+		// TODO fix the below part, as it's primitive idea. Put optimization here
 		Bid newbid = lOutcomeSpace.get(nCount).getBid();
 
 		nCount++;
@@ -78,6 +117,7 @@ public class Group5 extends AbstractNegotiationParty
 			nCount = 0;
 			newbid = lOutcomeSpace.get(nCount).getBid();
 		}
+		this.lastBid = newbid;
 		return new Offer(newbid);
 	}
 	
@@ -103,18 +143,30 @@ public class Group5 extends AbstractNegotiationParty
 	public void receiveMessage(AgentID sender, Action action) 
 	{
 		super.receiveMessage(sender, action);
-		// Here you hear other parties' messages
 
-		if(action instanceof Offer)
+		if(action instanceof Offer || sender != null)
 		{
-			this.lastBid = ((Offer)action).getBid();
+			if(action instanceof Offer)
+			{
+				this.lastBid = ((Offer)action).getBid();
+				if(!this.highestOfferedBids.containsKey(sender))
+					this.highestOfferedBids.put(sender, this.lastBid);
+				
+				if(getUtility(this.lastBid) > getUtility(this.highestOfferedBids.get(sender)))
+					this.highestOfferedBids.put(sender, this.lastBid);
+			}
 
 			List<Bid> bidList;
 			if(!this.bidHistory.containsKey(sender))
+			{
 				this.bidHistory.put(sender, bidList = new ArrayList<Bid>());
+				initializeUtilitySpace(sender);
+			}
 
 			bidList = this.bidHistory.get(sender);
 			bidList.add(lastBid);
+			
+			this.updateAgentModel(sender);
 		}
 	}
 
@@ -124,4 +176,115 @@ public class Group5 extends AbstractNegotiationParty
 		return "Group 5 Improved Agent";
 	}
 
+	private void initializeUtilitySpace(AgentID agent)
+	{
+		AdditiveUtilitySpace utilSpace = ((AdditiveUtilitySpace)this.utilitySpace.copy());
+
+		double w = 1.0 / nIssues;
+
+		// this technically sets all the issue weights equally at the beginning for a particular agent's utility space
+		for (Map.Entry<Objective, Evaluator> e : utilSpace.getEvaluators())
+		{
+			utilSpace.unlock((Objective)e.getKey());
+			((Evaluator)e.getValue()).setWeight(w);
+
+			// here all the values (discrete domain) of the particular issue is given equal weights, of 1 initially
+			Iterator<?> it = ((IssueDiscrete)e.getKey()).getValues().iterator();
+			while (it.hasNext())
+			{
+				ValueDiscrete value = (ValueDiscrete)it.next();
+				try 
+				{
+					((EvaluatorDiscrete)e.getValue()).setEvaluation(value, 1);
+				}
+				catch (Exception e1)
+				{
+					e1.printStackTrace();
+				}
+			}
+		}
+
+		this.lAgentUtilSpaces.put(agent, utilSpace);
+	}
+
+	private HashMap<Integer, Integer> bidChanges(AgentID agent)
+	{
+		if(this.bidHistory.get(agent).size() < 2)
+			return null;
+
+		HashMap<Integer, Integer> changes = new HashMap<Integer, Integer>();
+		List<Bid> lAgentBids = this.bidHistory.get(agent);
+
+		Bid agentLastBid = lAgentBids.get(lAgentBids.size() - 1);
+		Bid agentSecondLastBid = lAgentBids.get(lAgentBids.size() - 2);
+
+		for(Issue i : lIssue)
+		{
+			if( ((ValueDiscrete)agentLastBid.getValue(i.getNumber())).equals(
+				(ValueDiscrete)agentSecondLastBid.getValue(i.getNumber()) ) )
+				changes.put(i.getNumber(), 0);
+
+			else
+				changes.put(i.getNumber(), 1);
+		}
+
+		return changes;
+	}
+
+	private void updateAgentModel(AgentID agent)
+	{
+		if(this.bidHistory.get(agent).size() < 2)
+			return;
+
+		AdditiveUtilitySpace agentUtilSpace = lAgentUtilSpaces.get(agent);
+		HashMap<Integer, Integer> changes = bidChanges(agent);
+		int nChanged = 0;
+
+		// so we calculate the number of issues changed by the agent from its last bid
+		for(Issue i : lIssue)
+			if(changes.get(i.getNumber()) == 0)
+				nChanged++;
+
+		double fIncrement = 0.04;
+		double fCurrentSum = 1.0 + (fIncrement * nChanged);
+		double fMaxWeight = 1.0 - (nIssues * fIncrement / fCurrentSum);
+
+
+		// here we update the weights of each of the changed issues, and normalize the rest so that sum is 1
+		for(Issue i : lIssue)
+		{
+			double fUpdatedWt;
+			double fCurWt = agentUtilSpace.getWeight(i.getNumber());
+
+			if(changes.get(i.getNumber()) == 0 &&  fCurWt < fMaxWeight)
+				fUpdatedWt = (fCurWt + fIncrement) / fCurrentSum;
+			else
+				fUpdatedWt = fCurWt / fCurrentSum;
+
+			agentUtilSpace.setWeight(domain.getObjective(i.getNumber()), fUpdatedWt);
+		}
+
+		List<Bid> agentBidHistory = this.bidHistory.get(agent);
+		Bid agentLastBid = agentBidHistory.get(agentBidHistory.size() - 1);
+		Iterator<?> it = agentUtilSpace.getEvaluators().iterator();
+
+		while(it.hasNext())
+		{
+			Map.Entry<Objective, Evaluator> e = (Map.Entry)it.next();
+
+			ValueDiscrete value = (ValueDiscrete) agentLastBid.getValue(((IssueDiscrete)e.getKey()).getNumber());
+			int nOldValue;
+			try 
+			{
+				nOldValue = ((EvaluatorDiscrete)e.getValue()).getEvaluationNotNormalized(value);
+				((EvaluatorDiscrete)e.getValue()).setEvaluation(value, 1 + nOldValue);
+			}
+			catch (Exception e1)
+			{
+				e1.printStackTrace();
+			}
+		}
+
+		lAgentUtilSpaces.put(agent, agentUtilSpace);
+	}
 }
